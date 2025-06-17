d97fd0dbc671ac75e652329baf8c16d452b98382
RsShanks <81466881+RsShanks@users.noreply.github.com>
2025-06-17 08:36:11 +0200

    Add files via upload

---
+++ js/facture.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    $("#annuler").click(function (e) {
+        e.preventDefault();
+
+        window.location.href = 'panier.php';
+    })
+})
+//permet de mettre la commande comme payer
+$(document).ready(function () {
+    $("#envoyer").click(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            type: "GET",
+            url: "php/set_payer.php",
+            dataType: "json",
+            success: function (response) {
+                if (response.statu == 'ok') {
+                    //permet d'envoyer le mail
+            
+		
+                    $.ajax({
+                        type: "GET",
+                        url: "php/envoyerMail.php",
+                        dataType: "json",
+                        success: function (response1) {
+                         console.log("Réponse de envoyerMail.php :", response1);
+                            if (response1.statu == 'ok') {
+                                window.location.href = 'index.php';
+                            }
+                            else{
+                                console.log(response1.statu);
+                            }
+                        }
+                    });
+                    //
+                }
+                else{
+                    console.log(response.statu+"ici");
+                }
+            }
+        });
+    })
+})
